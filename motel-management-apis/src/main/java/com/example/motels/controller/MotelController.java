cc45a6610a4c92330e7318b99b42ab2381db86a7
VarunOjha <varunojha2013@gmail.com>
2025-08-02 21:35:28 -0500

    Adding to more routes

--- motel-management-apis/src/main/java/com/example/motels/controller/MotelController.java
+++ motel-management-apis/src/main/java/com/example/motels/controller/MotelController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/motels/chain/{motelChainId}/motels")
+@RequestMapping("/motelApi/v1/motels/chain/{motelChainId}/motels")
 public class MotelController {
 
     @Autowired
