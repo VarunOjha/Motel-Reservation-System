c08f9c8f75f649ab8241ab1ccccbba72cc612158
VarunOjha <varunojha2013@gmail.com>
2025-08-02 21:33:48 -0500

    Add port structure

--- reservation-apis/routes/router.go
+++ reservation-apis/routes/router.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RegisterRoutes(r *gin.Engine) {
-	v1 := r.Group("/api/v1")
+	v1 := r.Group("/reservationApi/v1")
 	{
 		v1.GET("/price-list", controllers.GetPriceList)
 		v1.POST("/reservation", controllers.PostReservation)
