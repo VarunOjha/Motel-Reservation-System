26647951500c86dc746721fd8fc28f619d20280b
VarunOjha <varunojha2013@gmail.com>
2025-08-10 16:45:20 -0500

    refactor a few things

--- motel-management-apis/src/main/java/com/example/motels/model/Address.java
+++ motel-management-apis/src/main/java/com/example/motels/model/Address.java
@@ -1,10 +1,5 @@
 package com.example.motels.model;
 
-import java.time.LocalDateTime;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import lombok.Data;
 
 @Data
